0beda77beaaab36fae9196202b252670d38fec49
Sandip Parmar <sandiprparmar92@gmail.com>
2018-12-13 14:36:40 -0800

    update

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Sandip' });
+router.get("/", function(req, res, next) {
+	res.render("index", { title: "Sandip" });
 });
 
-router.get('/:id', function(req, res,next){
-  res.send(req.params.id);
-})
+router.get("/:id", function(req, res, next) {
+	if (req.params.id <= 6) {
+		res.send(true);
+	} else {
+		res.send(false);
+	}
+});
 module.exports = router;
